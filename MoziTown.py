16cb3739c272ad6311e4d1bd41fd30d4c31d99a3
hajdupatya <hajdupatrik0826@gmail.com>
2024-03-07 07:41:07 +0100

    asd

--- MoziTown.py
+++ MoziTown.py
@@ -1,5 +1,13 @@
 from tkinter import *
+from tkinter import messagebox
+from ttkbootstrap import *
+
 root=Tk()
+root.configure(background="#181D31")
+root.geometry("1400x762")
+root.resizable(False,False)
+
+
 
 
 root.mainloop()
