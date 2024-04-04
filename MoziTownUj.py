90e875cb2ae6ffd2e729e93fdda9cc06a157f5f4
Hoffman Olivér <hoffmanoliver2006@gmail.com>
2024-04-04 07:31:41 +0200

    Átmásolás

---
+++ MoziTownUj.py
@@ -0,0 +1,221 @@
+from tkinter import *
+from tkinter.ttk import *
+from tkinter import messagebox
+from ttkbootstrap import *
+from tkinter import PhotoImage
+import datetime as dt
+from time import strftime
+import sqlite3
+
+con = sqlite3.connect("mozitown.db")
+cur = con.cursor()
+cur.execute("PRAGMA foreign_keys = ON;")
+try:
+    cur.execute("""CREATE TABLE termek(
+                Teremszam INT PRIMARY KEY,
+                Filmcim VARCHAR(32),
+                Filmev DATE,
+                Filmmufaj VARCHAR(16),
+                Fimhossz INT,
+                Teremkapacitas INT);
+    """)
+    cur.execute("""CREATE TABLE foglalasok(
+                Sorszam INT AUTO_INCREMENT PRIMARY KEY,
+                Keresztnev VARCHAR(32),
+                Vezeteknev VARCHAR(32),
+                Teremszam INT NOT NULL,
+                Szekszam INT);
+    """)
+except sqlite3.OperationalError or FileExistsError:
+    pass
+
+def foglal_ablak():
+    fog_ablak = Toplevel(root)
+    fog_ablak.geometry("1000x650")
+    fog_ablak.title("<Film>: Foglalás")
+
+    fcimkeret = LabelFrame(fog_ablak)
+    fcimkeret.grid(row=0, column=0, columnspan=2)
+    Label.columnconfigure(fcimkeret, 1, weight=1)
+    fcim = Label(fcimkeret, text="<Filmcím>", font=("Terminal", 20, "bold"), justify="center", anchor="center", width=90)
+    fcim.grid(row=0, column=0, pady=5, sticky="nesw")
+
+    fkeret = LabelFrame(fog_ablak)
+    fkeret.grid(row=1, column=1, padx=20, pady=20, sticky="nse")
+
+    fleiras = Label(fkeret, text="<Film leírása>", font=("Times", 12, "bold"))
+    fleiras.grid(row=0, column=0)
+    fszekek = Checkbutton(fkeret)
+    fszekek.grid(row=1, column=0)
+    ffoglal = Button(fkeret, text="Helyet foglalok")
+    ffoglal.grid(row=2, column=0)
+
+root = Window(themename="superhero")
+root.title("MoziTown")
+root.geometry("1400x762")
+root.resizable(False,False)
+root.configure(background="#181D31")
+date=dt.datetime.now()
+
+cimframe=Labelframe(root, border=0, width=1400, )
+cimframe.pack()
+
+cim=Label(cimframe, text="MoziTown",font=("Terminal","35","bold"),justify=CENTER)
+cim.grid(pady=20, padx=615, row=0,column=0, columnspan=2)
+
+def time():
+    string = strftime('%H:%M')
+    ora.config(text=string)
+    ora.after(1000, time)
+
+ora = Label(cimframe, font=('calibri', 20, 'bold'),foreground='white')
+ora.grid(row=0, padx=20,column=1)
+time()
+
+def hetfo():
+    mon.configure(bootstyle="warning")
+    tue.configure(bootstyle="warning-outline")
+    wed.configure(bootstyle="warning-outline")
+    thu.configure(bootstyle="warning-outline")
+    fri.configure(bootstyle="warning-outline")
+    sat.configure(bootstyle="warning-outline")
+    sun.configure(bootstyle="warning-outline")
+    musor.configure(text="Műsoron - hétfő")
+
+def kedd():
+    tue.configure(bootstyle="warning")
+    mon.configure(bootstyle="warning-outline")
+    wed.configure(bootstyle="warning-outline")
+    thu.configure(bootstyle="warning-outline")
+    fri.configure(bootstyle="warning-outline")
+    sat.configure(bootstyle="warning-outline")
+    sun.configure(bootstyle="warning-outline")
+    musor.configure(text="Műsoron - kedd")
+
+def szerda():
+    wed.configure(bootstyle="warning")
+    tue.configure(bootstyle="warning-outline")
+    mon.configure(bootstyle="warning-outline")
+    thu.configure(bootstyle="warning-outline")
+    fri.configure(bootstyle="warning-outline")
+    sat.configure(bootstyle="warning-outline")
+    sun.configure(bootstyle="warning-outline")
+    musor.configure(text="Műsoron - szerda")
+
+def csutotok():
+    thu.configure(bootstyle="warning")
+    tue.configure(bootstyle="warning-outline")
+    wed.configure(bootstyle="warning-outline")
+    mon.configure(bootstyle="warning-outline")
+    fri.configure(bootstyle="warning-outline")
+    sat.configure(bootstyle="warning-outline")
+    sun.configure(bootstyle="warning-outline")
+    musor.configure(text="Műsoron - csütörtök")
+
+
+def pentek():
+    fri.configure(bootstyle="warning")
+    tue.configure(bootstyle="warning-outline")
+    wed.configure(bootstyle="warning-outline")
+    thu.configure(bootstyle="warning-outline")
+    mon.configure(bootstyle="warning-outline")
+    sat.configure(bootstyle="warning-outline")
+    sun.configure(bootstyle="warning-outline")
+    musor.configure(text="Műsoron - péntek")
+
+def szombat():
+    sat.configure(bootstyle="warning")
+    tue.configure(bootstyle="warning-outline")
+    wed.configure(bootstyle="warning-outline")
+    thu.configure(bootstyle="warning-outline")
+    fri.configure(bootstyle="warning-outline")
+    mon.configure(bootstyle="warning-outline")
+    sun.configure(bootstyle="warning-outline")
+    musor.configure(text="Műsoron - szombat")
+
+def vasarnap():
+    sun.configure(bootstyle="warning")
+    tue.configure(bootstyle="warning-outline")
+    wed.configure(bootstyle="warning-outline")
+    thu.configure(bootstyle="warning-outline")
+    fri.configure(bootstyle="warning-outline")
+    sat.configure(bootstyle="warning-outline")
+    mon.configure(bootstyle="warning-outline")
+    musor.configure(text="Műsoron - vasárnap")
+
+
+style=ttk.Style().configure("frame_style", background="#181D31")
+
+
+napokfram=LabelFrame(root, border=0,)
+napokfram.pack(pady=10)
+
+mon=Button(napokfram,text="Hétfő", bootstyle="warning", command=hetfo)
+mon.grid(row=0, column=0,pady=(0,15),padx=15)
+tue=Button(napokfram,text="Kedd", bootstyle="warning-outline", command=kedd)
+tue.grid(row=0, column=1,pady=(0,15),padx=15)
+wed=Button(napokfram,text="Szerda", bootstyle="warning-outline", command=szerda)
+wed.grid(row=0, column=2,pady=(0,15),padx=15)
+thu=Button(napokfram,text="Csütörtök", bootstyle="warning-outline", command=csutotok)
+thu.grid(row=0, column=3,pady=(0,15),padx=15)
+fri=Button(napokfram,text="Péntek", bootstyle="warning-outline",command=pentek)
+fri.grid(row=0, column=4,pady=(0,15),padx=15)
+sat=Button(napokfram,text="Szombat", bootstyle="warning-outline", command=szombat)
+sat.grid(row=0, column=5,pady=(0,15),padx=15)
+sun=Button(napokfram,text="Vasárnap", bootstyle="warning-outline", command=vasarnap)
+sun.grid(row=0, column=6,pady=(0,15),padx=15)
+
+musor=Label(root,text="Műsoron - hétfő",font=('calibri', 25, 'bold'),background="#181D31")
+musor.pack(pady=(0,8))
+
+filmekframe=LabelFrame(root, style="frame_style", )
+filmekframe.pack()
+
+film1=LabelFrame(filmekframe, width=240, height=500,border=0)
+film1.grid(row=0,column=0, padx=20,pady=0)
+
+film2=LabelFrame(filmekframe,width=240, height=500,border=0)
+film2.grid(row=0,column=1, padx=20)
+
+film3=LabelFrame(filmekframe, width=240, height=500,border=0)
+film3.grid(row=0,column=2, padx=20)
+
+film4=LabelFrame(filmekframe,width=240, height=500,border=0)
+film4.grid(row=0,column=3, padx=20)
+
+dune = Canvas(film1, width=250, height=370, bg='white')
+dune.pack()
+img1 = ImageTk.PhotoImage(Image.open("dune.png"))  # PIL solution
+dune.create_image(0, 0, anchor=NW, image=img1)
+cim1=Label(film1,text="DŰNE - MÁSODIK RÉSZ",font=('calibri', 15, 'bold'))
+cim1.pack(pady=(6,0))
+buy1=Button(film1,text="Vásárlás", bootstyle="warning")
+buy1.pack(pady=6,padx=15,)
+
+most = Canvas(film2, width=250, height=370, bg='white')
+most.pack()
+img2 = ImageTk.PhotoImage(Image.open("most.png"))  # PIL solution
+most.create_image(0, 0, anchor=NW, image=img2)
+cim2=Label(film2,text="MOST VAGY SOHA!",font=('calibri', 15, 'bold'))
+cim2.pack(pady=(6,0))
+buy2=Button(film2,text="Vásárlás", bootstyle="warning")
+buy2.pack(pady=6,padx=15,)
+
+imadlak = Canvas(film3, width=250, height=370, bg='white')
+imadlak.pack()
+img3 = ImageTk.PhotoImage(Image.open("imadlak.png"))  # PIL solution
+imadlak.create_image(0, 0, anchor=NW, image=img3)
+cim3=Label(film3,text="IMÁDLAK UTÁLNI",font=('calibri', 15, 'bold'))
+cim3.pack(pady=(6,0))
+buy3=Button(film3,text="Vásárlás", bootstyle="warning")
+buy3.pack(pady=6,padx=15,)
+
+mehesz = Canvas(film4, width=250, height=370, bg='white')
+mehesz.pack()
+img4 = ImageTk.PhotoImage(Image.open("mehesz.png"))  # PIL solution
+mehesz.create_image(0, 0, anchor=NW, image=img4)
+cim4=Label(film4,text="A MÉHÉSZ",font=('calibri', 15, 'bold'))
+cim4.pack(pady=(6,0))
+buy4=Button(film4,text="Vásárlás", bootstyle="warning")
+buy4.pack(pady=6,padx=15,)
+root.mainloop()
